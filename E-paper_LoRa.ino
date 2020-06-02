087bdbde62d943ea5f2aa844fb607638d0e5b24a
Devin-Cooper <devin.sean.cooper@gmail.com>
2020-06-02 09:29:06 -0700

    added duplex and callback as well as moved to the new lora.h from arduino. Comms work without epaper display but when the display inits I no longer can receive

--- E-paper_LoRa.ino
+++ E-paper_LoRa.ino
@@ -2,7 +2,7 @@
 #include "epd2in9b.h"
 #include "imagedata.h"
 #include "epdpaint.h"
-#include "LoRa.h"
+#include <LoRa.h>
 
 
 #define COLORED     0
@@ -17,11 +17,37 @@
   #define DI0     7
   #define BAND    915E6 //868E6 
   #define PABOOST true 
+  
+String outgoing;              // outgoing message
+byte msgCount = 0;            // count of outgoing messages
+byte localAddress = 0xBB;     // address of this device
+byte destination = 0xFF;      // destination to send to
+long lastSendTime = 0;        // last send time
+int interval = 2000;          // interval between sends
+
 
 void setup() {
   // put your setup code here, to run once:
-  Serial.begin(9600);
-  Epd epd;
+  Serial.begin(9600);                   // initialize serial
+  //while (!Serial);
+
+  Serial.println("LoRa Duplex with callback");
+
+  // override the default CS, reset, and IRQ pins (optional)
+  LoRa.setPins(SS,RST,DI0);
+
+  if (!LoRa.begin(915E6)) {             // initialize ratio at 915 MHz
+    Serial.println("LoRa init failed. Check your connections.");
+    while (true);                       // if failed, do nothing
+  }
+  LoRa.setSpreadingFactor(8);           // ranges from 6-12,default 7 see API docs
+  LoRa.onReceive(onReceive);
+  LoRa.receive();
+  Serial.println("LoRa init succeeded.");
+  LoRa.sleep();
+
+//Init the display 
+ Epd epd;
 
   if (epd.Init() != 0) {
     Serial.print("e-Paper init failed");
@@ -69,53 +95,75 @@ void setup() {
   paint.DrawFilledCircle(32, 32, 30, COLORED);
   epd.SetPartialWindowRed(paint.GetImage(), 64, 200, paint.GetWidth(), paint.GetHeight());
 
-  */
+ 
 
   /* This displays the data from the SRAM in e-Paper module */
   epd.DisplayFrame();
 
   /* This displays an image 
   epd.DisplayFrame(IMAGE_BLACK, IMAGE_RED);
-*/
+
   /* Deep sleep */
  // epd.Sleep();
 
-
-delay(500);
-  LoRa.setPins(SS,RST,DI0);
-  if (!LoRa.begin(BAND,PABOOST )) {
-    Serial.println("Starting LoRa failed!");
-    while (1);
-  } 
+  
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-    // try to parse packet
-  int packetSize = LoRa.parsePacket();
-  if (packetSize) {
-    // received a packet
-    Serial.print("Received packet '");
-    // read packet
-    while (LoRa.available()) {
-      Serial.print((char)LoRa.read());
-    }
-    // print RSSI of packet
-    Serial.println("' with RSSI ");
-    int Rssi = (LoRa.packetRssi());
+  if (millis() - lastSendTime > interval) {
+    String message = "HeLoRa World!";   // send a message
+    sendMessage(message);
+    Serial.println("Sending " + message);
+    lastSendTime = millis();            // timestamp the message
+    interval = random(2000) + 1000;     // 2-3 seconds
+    LoRa.receive();                     // go back into receive mode
+  }
+}
 
-    LoRa.sleep();
-    Epd epd;
+void sendMessage(String outgoing) {
+  LoRa.beginPacket();                   // start packet
+  LoRa.write(destination);              // add destination address
+  LoRa.write(localAddress);             // add sender address
+  LoRa.write(msgCount);                 // add message ID
+  LoRa.write(outgoing.length());        // add payload length
+  LoRa.print(outgoing);                 // add payload
+  LoRa.endPacket();                     // finish packet and send it
+  msgCount++;                           // increment message ID
+}
 
-  unsigned char image[1024];
-  Paint paint(image, 128, 18);    //width should be the multiple of 8 
-  paint.Clear(UNCOLORED);
-  paint.DrawStringAt(0, 0, "i dont know", &Font12, COLORED);
-  epd.SetPartialWindowBlack(paint.GetImage(), 24, 32, paint.GetWidth(), paint.GetHeight());
-    /* This displays the data from the SRAM in e-Paper module */
-  epd.DisplayFrame();
-    /* Deep sleep */
-  epd.Sleep();
+void onReceive(int packetSize) {
+  if (packetSize == 0) return;          // if there's no packet, return
+
+  // read packet header bytes:
+  int recipient = LoRa.read();          // recipient address
+  byte sender = LoRa.read();            // sender address
+  byte incomingMsgId = LoRa.read();     // incoming msg ID
+  byte incomingLength = LoRa.read();    // incoming msg length
+
+  String incoming = "";                 // payload of packet
+
+  while (LoRa.available()) {            // can't use readString() in callback, so
+    incoming += (char)LoRa.read();      // add bytes one by one
   }
 
+  if (incomingLength != incoming.length()) {   // check length for error
+    Serial.println("error: message length does not match length");
+    return;                             // skip rest of function
+  }
+
+  // if the recipient isn't this device or broadcast,
+  if (recipient != localAddress && recipient != 0xFF) {
+    Serial.println("This message is not for me.");
+    return;                             // skip rest of function
+  }
+
+  // if message is for this device, or broadcast, print details:
+  Serial.println("Received from: 0x" + String(sender, HEX));
+  Serial.println("Sent to: 0x" + String(recipient, HEX));
+  Serial.println("Message ID: " + String(incomingMsgId));
+  Serial.println("Message length: " + String(incomingLength));
+  Serial.println("Message: " + incoming);
+  Serial.println("RSSI: " + String(LoRa.packetRssi()));
+  Serial.println("Snr: " + String(LoRa.packetSnr()));
+  Serial.println();
 }
